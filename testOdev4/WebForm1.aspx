94110ae43b88337c95f163d320579c5764a63d2e
gor2em <grkem.squash99@gmail.com>
2020-06-06 15:21:36 +0300

    C# ASP.NET ile test uygulaması

---
+++ testOdev4/WebForm1.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="testOdev4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        #main {
+            margin: 30px auto;
+            width: 1200px;
+        }
+
+        #left-side {
+            width: 800px;
+            float: left;
+        }
+
+        #right-side {
+            width: 400px;
+            float: left;
+            background: #e8e8e8;
+            text-align: center;
+            padding-top: 10px;
+        }
+
+        input[type="submit"] {
+            border: none;
+            padding: 20px;
+            text-align: center;
+            cursor: pointer;
+            border:1px solid #ccc;
+            color:#000;
+        }
+        /*#00ff90*/
+
+        .btnHover:hover {
+            transition-duration: 200ms;
+            border:1px solid #000;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div id="main">
+            <div id="left-side">
+                <asp:Button ID="btn_basla" runat="server" Text="Başlat" OnClick="btn_basla_Click" />
+                <br />
+                <br />
+                <asp:Panel ID="Panel1" runat="server" Width="795">
+
+                    <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+                        <Triggers>
+                            <asp:AsyncPostBackTrigger ControlID="Timer1" EventName="Tick" />
+                        </Triggers>
+                        <ContentTemplate>
+                            <asp:Label ID="lbl_dakika" runat="server" Text=""></asp:Label>
+                            <asp:Label ID="lbl_saniye" runat="server" Text=""></asp:Label>
+                        </ContentTemplate>
+                    </asp:UpdatePanel>
+
+                    <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Timer1_Tick" Enabled="False">
+                    </asp:Timer>
+
+                    <asp:TextBox ID="TextBox1" runat="server" Height="150px" Width="434px" TextMode="MultiLine"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Label ID="Label2" runat="server" Text="Doğru:"></asp:Label><asp:Label ID="lbl_dogru" runat="server" Text=""></asp:Label><br />
+                    <asp:Label ID="Label3" runat="server" Text="Yanlış:"></asp:Label><asp:Label ID="lbl_yanlis" runat="server" Text=""></asp:Label><br />
+                    <asp:Label ID="Label1" runat="server" Text="Soru:"></asp:Label><asp:Label ID="lbl_soru" runat="server" Text=""></asp:Label><asp:Label ID="lbl_topSoru" runat="server" Text=""></asp:Label><br />
+
+
+                    <br />
+                    <br />
+                    <asp:Button ID="btn1" runat="server" Text="A" CssClass="btnHover" OnClick="btn1_Click" />
+                    <br />
+                    <br />
+                    <asp:Button ID="btn2" runat="server" Text="B" CssClass="btnHover" OnClick="btn2_Click" />
+                    <br />
+                    <br />
+                    <asp:Button ID="btn3" runat="server" Text="C" CssClass="btnHover" OnClick="btn3_Click" />
+                    <br />
+                    <br />
+                    <asp:Button ID="btn4" runat="server" Text="D" CssClass="btnHover" OnClick="btn4_Click" />
+
+                </asp:Panel>
+            </div>
+            <div id="right-side">
+                <asp:Button ID="btn_cevap" runat="server" Text="Cevabı Göster" OnClick="btn_cevap_Click"  />
+                <br /><br />
+                <asp:Button ID="btn_sonuc" runat="server" Text="Sonuç" OnClick="btn_sonuc_Click" Visible="False" />
+                <br />
+                <asp:Label ID="lbl_sonuc" runat="server" Text="0" Visible="false"></asp:Label><br />
+                <asp:Label ID="lbl_sureTOP" runat="server" Text=""></asp:Label><br />
+                <asp:Label ID="lbl_cevap" runat="server" Text="" Visible="false"></asp:Label><br />
+
+                <h1>görkem acar</h1>
+
+            </div>
+        </div>
+
+
+
+    </form>
+</body>
+</html>
---
+++ testOdev4/WebForm1.aspx.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace testOdev4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        SqlConnection baglan = new SqlConnection("Data Source=GLORY\\GLORY;Initial Catalog=sorular;Integrated Security=True");
+        private static int soruSayac = 0, dogruSayac = 0, yanlisSayac = 0, toplamSoru = 7, saniye = 60, dakika = 2,sureTOP=0;
+        private static double hesap = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            bgClear();
+            lbl_soru.Text = soruSayac.ToString();
+            lbl_dogru.Text = dogruSayac.ToString();
+            lbl_yanlis.Text = yanlisSayac.ToString();
+            lbl_topSoru.Text = "/" + toplamSoru.ToString();
+     
+
+            if (toplamSoru == soruSayac)
+            {
+                btn_basla.Visible = false;
+                Timer1.Enabled = false;
+                Panel1.Enabled = false;
+                btn_sonuc.Visible = true;
+                btn_cevap.Enabled = false;
+            }
+        }
+        void bgClear() { btn1.BackColor = Color.Empty; btn2.BackColor = Color.Empty; btn3.BackColor = Color.Empty; btn4.BackColor = Color.Empty; }
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            saniye = saniye - 1;
+            sureTOP += 1;
+            lbl_saniye.Text = Convert.ToString(saniye);
+            lbl_dakika.Text = Convert.ToString(dakika - 1);
+            if (saniye == 0)
+            {
+                dakika = dakika - 1;
+                lbl_dakika.Text = Convert.ToString(dakika);
+                saniye = 60;
+            }
+
+            if (lbl_dakika.Text == "-1")
+            {
+                Timer1.Enabled = false;
+                lbl_saniye.Text = "0";
+                lbl_dakika.Text = "0";
+                Panel1.Visible = false;
+                btn_basla.Text = "süren doldu";
+            }
+        }
+
+        protected void btn_cevap_Click(object sender, EventArgs e)
+        {
+            btn_basla.Enabled = true;
+            if (btn1.Text==lbl_cevap.Text)
+            {
+                btn1.BackColor = Color.DarkCyan;
+                Panel1.Enabled = false;
+                yanlisSayac++;
+            }
+             if (btn2.Text == lbl_cevap.Text)
+            {
+                btn2.BackColor = Color.DarkCyan;
+                Panel1.Enabled = false;
+                yanlisSayac++;
+            }
+             if (btn3.Text == lbl_cevap.Text)
+            {
+                btn3.BackColor = Color.DarkCyan;
+                Panel1.Enabled = false;
+                yanlisSayac++;
+            }
+            if(btn4.Text==lbl_cevap.Text)
+            {
+                btn4.BackColor = Color.DarkCyan;
+                Panel1.Enabled = false;
+                yanlisSayac++;
+            }
+        }
+
+        protected void btn_sonuc_Click(object sender, EventArgs e)
+        {
+            lbl_sonuc.Visible = true;
+            hesap = ((Convert.ToDouble(toplamSoru) - Convert.ToDouble(yanlisSayac))) / (Convert.ToDouble(toplamSoru));
+            hesap = Math.Round(hesap * 100);
+            lbl_sonuc.Text = "Doğruluk oranı:%" + hesap.ToString();
+            //
+            lbl_sureTOP.Text = sureTOP.ToString()+" saniye içerisinde çözdünüz!";
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+
+            btn_basla.Enabled = true;
+            if (btn1.Text == lbl_cevap.Text)
+            {
+                dogruSayac++;
+                lbl_dogru.Text = dogruSayac.ToString();
+                btn1.BackColor = Color.Green;
+
+            }
+            else
+            {
+                yanlisSayac++;
+                lbl_yanlis.Text = yanlisSayac.ToString();
+                btn1.BackColor = Color.Red;
+            }
+
+        }
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            btn_basla.Enabled = true;
+            if (btn2.Text == lbl_cevap.Text)
+            {
+                dogruSayac++;
+                lbl_dogru.Text = dogruSayac.ToString();
+                btn2.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlisSayac++;
+                lbl_yanlis.Text = yanlisSayac.ToString();
+                btn2.BackColor = Color.Red;
+            }
+        }
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+
+            btn_basla.Enabled = true;
+            if (btn3.Text == lbl_cevap.Text)
+            {
+                dogruSayac++;
+                lbl_dogru.Text = dogruSayac.ToString();
+                btn3.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlisSayac++;
+                lbl_yanlis.Text = yanlisSayac.ToString();
+                btn3.BackColor = Color.Red;
+            }
+        }
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+
+            btn_basla.Enabled = true;
+            if (btn4.Text == lbl_cevap.Text)
+            {
+                dogruSayac++;
+                lbl_dogru.Text = dogruSayac.ToString();
+                btn4.BackColor = Color.Green;
+            }
+            else
+            {
+                yanlisSayac++;
+                lbl_yanlis.Text = yanlisSayac.ToString();
+                btn4.BackColor = Color.Red;
+            }
+
+        }
+
+        protected void btn_basla_Click(object sender, EventArgs e)
+        {
+            Panel1.Enabled = true;
+            TextBox1.Enabled = false;
+            btn_basla.Enabled = false;
+            Timer1.Enabled = true;
+            soruSayac++;
+
+            lbl_soru.Text = soruSayac.ToString();
+            btn_basla.Text = "Sonraki";
+
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("SELECT * FROM soru1 WHERE id=" + soruSayac, baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                btn1.Text = (oku["a"].ToString());
+                btn2.Text = (oku["b"].ToString());
+                btn3.Text = (oku["c"].ToString());
+                btn4.Text = (oku["d"].ToString());
+                TextBox1.Text = (oku["soru"].ToString());
+                lbl_cevap.Text = (oku["cevap"].ToString());
+            }
+            baglan.Close();
+        }
+
+    }
+}
